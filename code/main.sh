fba9b6bc3824d6310c11c5df9e525340f0b7e774
Trung Vu <vptrung@yahoo.com>
2020-12-24 17:08:25 +0700

    fix git dir

--- code/main.sh
+++ code/main.sh
@@ -1,31 +1,31 @@
 #!/bin/sh
 
-# cmd=$(git remote show origin | grep "server")
-# cmd=${cmd#*(}
-# cmd=${cmd%)*}
+cmd=$(git remote show origin | grep "server")
+cmd=${cmd#*(}
+cmd=${cmd%)*}
 
-# if [ "$cmd" == "local out of date" ]
-# then
-#     cd /Users/defold/Projects/ToolGenBtn/code
-#     open speekStart.app
-#     cd /Users/defold/Projects/ToolGenBtn
-#     git pull origin server
+if [ "$cmd" == "local out of date" ]
+then
+    cd /Users/defold/Projects/ToolGenBtn/code
+    open speekStart.app
+    cd /Users/defold/Projects/ToolGenBtn
+    git pull origin server
     
-#     sleep 15 
+    sleep 15 
 
-#     cd /Users/defold/Projects/ToolGenBtn/code
+    cd /Users/defold/Projects/ToolGenBtn/code
     
 
-#     rm -f ../temp/psd/*
-#     rm -f ../temp/png/*
+    rm -f ../temp/psd/*
+    rm -f ../temp/png/*
 
-#     python3 JSON2CSV.py
+    python3 JSON2CSV.py
 
-#     open ../input/*.psd
-#     open newAction.app
-#     wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
+    open ../input/*.psd
+    open newAction.app
+    wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
 
-#     python3 combinePNG.py
+    python3 combinePNG.py
 
     cd /Users/defold/Projects/ToolGenBtn
     git add \.
@@ -37,29 +37,29 @@
 
     sleep 10
 
-#     open speekEnd.app
+    open speekEnd.app
 
-# elif [ "$cmd" == "up to date" ]
-# then
-#     echo "nothing to do"
-# fi
-
-
+elif [ "$cmd" == "up to date" ]
+then
+    echo "nothing to do"
+fi
 
 
 
-# cd /Users/defold/Projects/ToolGenBtn/code
-# open speekStart.app
 
-# rm -f ../temp/psd/*
-# rm -f ../temp/png/*
 
-# python3 JSON2CSV.py
+cd /Users/defold/Projects/ToolGenBtn/code
+open speekStart.app
 
-# open ../input/*.psd
-# open newAction.app
-# wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
+rm -f ../temp/psd/*
+rm -f ../temp/png/*
 
-# python3 combinePNG.py
+python3 JSON2CSV.py
 
-# open speekEnd.app
+open ../input/*.psd
+open newAction.app
+wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
+
+python3 combinePNG.py
+
+open speekEnd.app
