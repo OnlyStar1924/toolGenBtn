80b3a8f8d6d9c7a27fe2b9141f9b832783d4414b
Trung Vu <vptrung@yahoo.com>
2020-12-24 17:11:20 +0700

    fix git dir lan 2

--- code/main.sh
+++ code/main.sh
@@ -43,23 +43,3 @@ elif [ "$cmd" == "up to date" ]
 then
     echo "nothing to do"
 fi
-
-
-
-
-
-cd /Users/defold/Projects/ToolGenBtn/code
-open speekStart.app
-
-rm -f ../temp/psd/*
-rm -f ../temp/png/*
-
-python3 JSON2CSV.py
-
-open ../input/*.psd
-open newAction.app
-wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
-
-python3 combinePNG.py
-
-open speekEnd.app
