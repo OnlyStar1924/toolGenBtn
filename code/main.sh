8c0bc11bdbd01f36b64246344acba6eef2674070
Trung Vu <vptrung@yahoo.com>
2020-12-24 17:07:17 +0700

    new commit for Thu Dec 24 17:07:17 +07 2020

--- code/main.sh
+++ code/main.sh
@@ -1,32 +1,33 @@
 #!/bin/sh
 
-cmd=$(git remote show origin | grep "server")
-cmd=${cmd#*(}
-cmd=${cmd%)*}
+# cmd=$(git remote show origin | grep "server")
+# cmd=${cmd#*(}
+# cmd=${cmd%)*}
 
-if [ "$cmd" == "local out of date" ]
-then
-    cd /Users/defold/Projects/ToolGenBtn/code
-    open speekStart.app
+# if [ "$cmd" == "local out of date" ]
+# then
+#     cd /Users/defold/Projects/ToolGenBtn/code
+#     open speekStart.app
+#     cd /Users/defold/Projects/ToolGenBtn
+#     git pull origin server
+    
+#     sleep 15 
+
+#     cd /Users/defold/Projects/ToolGenBtn/code
+    
+
+#     rm -f ../temp/psd/*
+#     rm -f ../temp/png/*
+
+#     python3 JSON2CSV.py
+
+#     open ../input/*.psd
+#     open newAction.app
+#     wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
+
+#     python3 combinePNG.py
+
     cd /Users/defold/Projects/ToolGenBtn
-    git pull origin server
-    
-    sleep 15 
-
-    cd /Users/defold/Projects/ToolGenBtn/code
-    
-
-    rm -f ../temp/psd/*
-    rm -f ../temp/png/*
-
-    python3 JSON2CSV.py
-
-    open ../input/*.psd
-    open newAction.app
-    wc -l ../temp/inputText.csv | awk '{ temp = $1 ; while (temp > 0) { system("sleep 1"); temp-- } }' 
-
-    python3 combinePNG.py
-
     git add \.
     sleep 1
     git commit -m "new commit for `date`"
@@ -36,12 +37,12 @@ then
 
     sleep 10
 
-    open speekEnd.app
+#     open speekEnd.app
 
-elif [ "$cmd" == "up to date" ]
-then
-    echo "nothing to do"
-fi
+# elif [ "$cmd" == "up to date" ]
+# then
+#     echo "nothing to do"
+# fi
 
 
 
